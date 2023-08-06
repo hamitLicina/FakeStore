07581d55a5a46773d33b658298d7120833439bb8
hamitLicina <hamitlicina@gmail.com>
2023-08-06 22:28:29 +0300

    some adjustments added

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -10,7 +10,8 @@ function Header() {
   return (
     <div className='header-container'>
         <h3 className='logo'>Fake Store</h3>
-        <AiOutlineShoppingCart className='shopping-car-icon' placeholder="5" />
+        <AiOutlineShoppingCart className='shopping-car-icon' />
+        <span className='number'>5</span>
     </div>
   )
 }

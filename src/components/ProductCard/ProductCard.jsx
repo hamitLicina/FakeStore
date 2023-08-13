ecb9d10a692606d75ed5ce475678f37b3bb34090
hamitLicina <hamitlicina@gmail.com>
2023-08-13 12:22:01 +0300

    Some CSS addded

--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -1,13 +1,20 @@
 import React from 'react'
 import './ProductCard.css'
 
+ // I want to make first letter uppercase
+ const capitalizeFirstLetter = (string) => {
+
+  return string.charAt(0).toUpperCase() + string.slice(1);
+};
+
+
 function ProductCard({product}) {
   return (
     <div className='product-card'>
         <img src={product.image} />
-        <p>{product.title}</p>
-        <p>{product.category}</p>
-        <p>{product.price}€ </p>
+        <p style={{fontWeight:'bold'}}>{product.title}</p>
+        <p style={{color:'var(--gray)'}}>{capitalizeFirstLetter(product.category)}</p>
+        <p style={{fontWeight:'bold'}}>{product.price}€ </p>
     </div>
   )
 }

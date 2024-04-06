62784d986479911537a4016ee6224f1044f57b58
hamitLicina <hamitlicina@gmail.com>
2024-04-06 06:10:04 +0300

    Like component added

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,6 +6,8 @@ import { AiOutlineShoppingCart } from "react-icons/ai";
 import { Link } from 'react-router-dom';
 import { CartContext } from '../../contexts/CartContext';
 import logo from '../../assets/hamitLicinaLogo.jpg'
+import { GiChainedHeart } from "react-icons/gi";
+//<GiChainedHeart />
 
 
 
@@ -20,6 +22,7 @@ function Header() {
       <Link to="/"><h3 className='logo'>Hamit's Store <img src={logo} className='hamitLicinaLogo' alt="Hamit Licina's Logo" /></h3></Link>
       <div className='cart-wrapper'>
         <Link to="/checkout">
+          <GiChainedHeart className='likes-icon' />
           <AiOutlineShoppingCart className='shopping-car-icon' />
           <p className='cart-number'>{cart.length}</p>
         </Link>
